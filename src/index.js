f4ff23b83adbaa26ddbe4c297d9c22799923bd27
PetroKasperskyi <kaspersky28061993@gmail.com>
2023-09-14 12:12:49 +0300

    commit 001

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import 'src/index.css'
 import { GlobalStyle } from 'components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
      <GlobalStyle />
     <App />
-
+</BrowserRouter>
   </React.StrictMode>
 );
